19dd8c3b797d41a6c5a32712773447a843cdfdc0
miniscandal <oscar01dev@gmail.com>
2025-10-11 15:26:06 -0600

    update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
-	plugins: [preact()],
-	resolve: {
-		alias: {
-			'@core-services': '/src/core/services',
-			'@assets': '/src/assets',
-			'@shared-components': '/src/shared/components',
-			'@shared-contexts': '/src/shared/contexts',
-			'@shared-custom-hooks': '/src/shared/custom-hooks',
-			'@shared-constants': '/src/shared/constants'
-		}
-	}
+    plugins: [preact()],
+    resolve: {
+        alias: {
+            '@core-services': '/src/core/services',
+            '@assets': '/src/assets',
+            '@shared-components': '/src/shared/components',
+            '@shared-contexts': '/src/shared/contexts',
+            '@shared-custom-hooks': '/src/shared/custom-hooks',
+            '@shared-constants': '/src/shared/constants',
+        },
+    },
 });
