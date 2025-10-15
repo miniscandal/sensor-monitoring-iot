c86b0c25943529ee81a908c8dc947cec286aa77b
miniscandal <oscar01dev@gmail.com>
2025-10-15 15:59:34 -0600

    style: change double quotes to single quotes in imports

--- src/feat-connected-devices-monitoring/index.tsx
+++ src/feat-connected-devices-monitoring/index.tsx
@@ -1,5 +1,5 @@
-import { ConnectedDeviceCount } from "./components/organisms/connected-device-count";
-import { ConnectedDeviceView } from "./components/organisms/connected-device-view";
+import { ConnectedDeviceCount } from './components/organisms/connected-device-count';
+import { ConnectedDeviceView } from './components/organisms/connected-device-view';
 
 
 function FeatConnectedDeviceMonitoring() {
