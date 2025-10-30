5d0d34d268788ec58e85fa12dbbbdf2bdf2a284b
miniscandal <oscar01dev@gmail.com>
2025-10-29 18:53:46 -0600

    refactor: raname prop

--- src/shared/components/molecules/property-item/index.jsx
+++ src/shared/components/molecules/property-item/index.jsx
@@ -5,11 +5,11 @@ import { LabelValue } from '@shared-components/molecules/label-value';
 import './style.css';
 
 
-function PropertyItem({ label = 'property item', svgIcon, value = 'N/A', size = 'resizable' }) {
+function PropertyItem({ svgIconName, label = 'property item', value = 'N/A', size = 'resizable' }) {
 
     return (
         <section class="parameter" data-size={size}>
-            <SvgIcon svg={svgIcon} />
+            <SvgIcon name={svgIconName} />
             <LabelValue label={label} value={value} direction="column" />
         </section>
     );
