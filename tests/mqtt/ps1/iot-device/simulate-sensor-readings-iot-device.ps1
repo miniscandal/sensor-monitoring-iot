75be6a1668b25d092a9304faf3b67475c74c4cff
miniscandal <oscar01dev@gmail.com>
2025-11-10 18:53:16 -0600

    chore: remove unused bat tests

--- tests/mqtt/ps1/iot-device/simulate-sensor-readings-iot-device.ps1
+++
@@ -1,9 +0,0 @@
-mosquitto_pub.exe -h localhost -p 1883 -t "web-iot-control-panel" -m '{
-    "deviceId": "01",
-    "statusCode": 201,
-    "sensorReadings": {
-        "temperature": 25.3,
-        "humidity": 60
-    },
-    "timestamp": "2025-10-18T14:32:00Z"
-}'
