061dd6f0658cfd8e06278cf3df132cf3a32957d4
miniscandal <oscar01dev@gmail.com>
2025-11-10 19:09:22 -0600

    feat: add useMqttClientMessageTracking hook for tracking MQTT messages

---
+++ src/shared/custom-hooks/mqtt-client/use-mqtt-client-message-tracking/index.ts
@@ -0,0 +1,33 @@
+import { useState } from 'preact/hooks';
+
+import { useMQTTObserver } from '../use-mqtt-observer';
+
+import { MQTT_CLIENT_EVENT_MESSAGE } from '@shared-constants/mqtt-client-events';
+import { MQTT_CLIENT_EVENT_OFFLINE } from '@shared-constants/mqtt-client-events';
+import { MQTT_CLIENT_MESSAGE_TRACKING } from '@shared-constants/mqtt-client';
+
+
+function useMqttClientMessageTracking() {
+    const [messages, setMessages] = useState([]);
+
+    useMQTTObserver({
+        events: [MQTT_CLIENT_EVENT_MESSAGE, MQTT_CLIENT_EVENT_OFFLINE],
+        statusCodes: [MQTT_CLIENT_MESSAGE_TRACKING],
+        observer: (event, message) => {
+            if (event === MQTT_CLIENT_EVENT_OFFLINE) {
+                setMessages([]);
+            }
+
+            setMessages((prevState) => (
+                [
+                    ...prevState,
+                    message,
+                ]
+            ));
+        },
+    });
+
+    return messages;
+}
+
+export { useMqttClientMessageTracking };
