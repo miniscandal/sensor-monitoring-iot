f6ebbfd15918c73c30b2172140956f1e57af9486
miniscandal <oscar01dev@gmail.com>
2025-11-10 19:01:06 -0600

    feat: implement device data tracking logic in DataTracking organism

--- src/feat-device-data-tracking/components/organisms/data-tracking/index.tsx
+++ src/feat-device-data-tracking/components/organisms/data-tracking/index.tsx
@@ -1,12 +1,20 @@
 import './style.css';
 
+import { useMqttClientMessageTracking } from '@shared-custom-hooks/mqtt-client/use-mqtt-client-message-tracking';
+
 
 function DataTracking() {
+    const messages = useMqttClientMessageTracking();
+
+    const data = messages.map(message => JSON.stringify(message));
+
 
     return (
         <section class="data_tracking">
             <div class="data_tracking__div">
-                <div class="data_tracking__scroll"></div>
+                <div class="data_tracking__scroll">
+                    {data}
+                </div>
             </div>
         </section>
     );
