2c3f5fef0ed8eff1857f97e2695854a054d1ec2d
miniscandal <oscar01dev@gmail.com>
2025-11-26 18:26:32 -0600

    feat(device-management): enhance IoTDeviceControlPanel with selected state and refactored styles

--- src/features/device-management/components/molecules/iot-device-control-panel/index.tsx
+++ src/features/device-management/components/molecules/iot-device-control-panel/index.tsx
@@ -3,21 +3,24 @@ import { SvgIcon } from '@shared-components/atoms/svg-icon';
 import './style.css';
 
 
-function IoTDeviceControlPanel({ iotDeviceState = {} }) {
-    const { isActive = true } = iotDeviceState;
+function IoTDeviceControlPanel({ isActive = !true, isSelected = true }) {
+    const iconStatus = isActive ? 'stopCircle' : 'playCircle';
+    const classStatus = isActive ? 'active' : 'paused';
+    const transmission = isActive ? 'pause' : 'activate';
+
 
     return (
-        <ul class="iot-device-control-panel">
+        <ul class={`iot-device-control-panel ${isSelected ? "selected" : ""}`}>
             <li class={`item-terminal`} data-action="terminal">
                 <SvgIcon name="terminal" size="small" enableHover={true} />
             </li>
-            <li class={`item-mode-active-paused ${isActive ? 'active' : 'paused'}`} data-action="modeOffOn">
-                <SvgIcon name="modeOffOn" size="small" enableHover={true} />
+            <li class={`item-mode-active-paused ${classStatus}`} data-action={transmission}>
+                <SvgIcon name={iconStatus} size="small" enableHover={true} />
             </li>
-            <li class={`item-analytics`}>
+            <li class={`item-analytics`} data-action="analytics">
                 <SvgIcon name="analytics" size="small" enableHover={true} />
             </li>
-        </ul>
+        </ul >
     );
 }
 
