c728743223bd78c1d517af5fe9f80fa18e664a92
miniscandal <oscar01dev@gmail.com>
2025-12-01 04:39:05 -0600

    refactor: remove LiveDataFeed references and add registered color variable

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,6 @@ import { MqttClientSingleton } from '@core-services/mqtt-client';
 import { MqttClientStatus } from './features/mqtt-client-status/components/templates/mqtt-client-status';
 import { TopicSubscription } from './features/topics-subscription/components/templates/topic-subscription';
 import { DeviceManagement } from './features/device-management/components/templates/device-management';
-// import { LiveDataFeed } from './features/live-data-feed/components/templates/live-data-feed';
 
 import { Header } from '@shared-components/organisms/header';
 
@@ -33,7 +32,6 @@ export function App() {
                 <MqttClientStatus />
                 <TopicSubscription />
                 <DeviceManagement />
-                {/* <LiveDataFeed /> */}
             </main>
         </>
     );
