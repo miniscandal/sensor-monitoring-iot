4e36d9d6cadc780826fca386311d6f98b1cd2b9a
miniscandal <oscar01dev@gmail.com>
2026-01-13 14:55:14 -0600

    feat(mqtt-client): extend use-event-subject-subscribe hook with operationCodes support

--- src/shared/hooks/mqtt-client/use-event-subject-subscribe/index.ts
+++ src/shared/hooks/mqtt-client/use-event-subject-subscribe/index.ts
@@ -3,11 +3,12 @@ import { useEffect } from 'preact/hooks';
 import { mqttClientEventSubject } from '@core-services/mqtt-client-event-subject';
 
 
-function useMqttClientEventSubjectSubscribe({ events, statusCodes, observer }) {
+function useMqttClientEventSubjectSubscribe({ events, statusCodes, operationCodes, observer }) {
     useEffect(() => {
         mqttClientEventSubject.subscribe({
             events,
             statusCodes,
+            operationCodes,
             observer,
         });
 
