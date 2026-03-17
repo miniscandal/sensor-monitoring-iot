25a9b65fbb98724496879d8770ad880c020c0362
miniscandal <oscar01dev@gmail.com>
2026-03-17 01:54:46 -0600

    docs(use-events): document absence of deps in useEffect

--- src/shared/hooks/mqtt-client/use-events/index.js
+++ src/shared/hooks/mqtt-client/use-events/index.js
@@ -1,3 +1,10 @@
+/**
+ * Hook to manage MQTT events using the Observer pattern.
+ * An empty dependency array is used in useEffect because the observers 
+ * are considered static for the lifecycle of this component.
+ */
+
+
 import { useEffect } from 'preact/hooks';
 
 import { mqttClientEventSubject } from '@core-services/mqtt-client-event-subject';
@@ -14,6 +21,7 @@ function useMqttClientEvents({ events, statusCodes, operationCodes, listener }) 
 
 
         return () => mqttClientEventSubject.unsubscribe(observerId);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 }
 
