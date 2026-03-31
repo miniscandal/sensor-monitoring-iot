d1d290ee50eb5925f587e589dc78bbe70bcbf9fc
miniscandal <oscar01dev@gmail.com>
2026-03-31 16:36:36 -0600

    feat(mqtt-client-messages): add feed table constants

---
+++ src/features/mqtt-client-messages/constants/feed-table.js
@@ -0,0 +1,10 @@
+const HEADERS = ['timestamp', 'device_id', 'status', 'message_summary'];
+
+const COLUMNS = [
+    { span: 1, class: 'table__column--timestamp' },
+    { span: 1, class: 'table__column--device-id' },
+    { span: 1, class: 'table__column--status' },
+    { span: 1, class: 'table__column--message-summary' },
+];
+
+export { COLUMNS, HEADERS };
