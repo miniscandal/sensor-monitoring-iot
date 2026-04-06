62eaa7b78d06f63a52e517f9c7760d2af8c5f9a3
miniscandal <oscar01dev@gmail.com>
2026-04-06 04:40:55 -0600

    feat(environmental-node-messages): normalize mock messages keys to camelCase

--- src/features/environmental-node-messages/components/page/environmental-node-messages/index.jsx
+++ src/features/environmental-node-messages/components/page/environmental-node-messages/index.jsx
@@ -2,11 +2,15 @@ import { NodeMessagesMonitor } from '../../templates/node-messages-monitor';
 
 import messageCollection from '@mocks/environmental-nodes/node-collection.json';
 
+import { deepCamel } from '@shared-utils/deep-camel';
+
 import './style.css';
 
 
 function EnvironmentalNodeMessagesPage() {
-    const messages = [...messageCollection, ...messageCollection, ...messageCollection];
+    const rawMessages = [...messageCollection, ...messageCollection, ...messageCollection];
+
+    const messages = rawMessages.map(deepCamel);
 
 
     return (
