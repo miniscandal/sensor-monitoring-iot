44d03ba722c0bc2ec43d76d6c5d90a2fc7bbb28e
miniscandal <oscar01dev@gmail.com>
2026-04-07 23:05:44 -0600

    chore(config): add @domain alias and update paths
    - Removed deprecated `baseUrl` from jsconfig.json
    - Updated paths to use relative `./src/...`
    - Added new aliases: @core-interfaces, @domain, and @infrastructure
    - Synced vite.config.js with the new aliases

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
     resolve: {
         alias: {
             '@core-constants': '/src/core/constants',
+            '@core-interfaces': '/src/core/interfaces',
             '@core-mqtt': '/src/core/mqtt',
             '@core-services': '/src/core/services',
-
+            '@domain': '/src/domain',
+            '@infrastructure': '/src/infrastructure',
             '@features': '/src/features',
             '@assets': '/src/assets',
             '@mocks': '/src/mocks',
