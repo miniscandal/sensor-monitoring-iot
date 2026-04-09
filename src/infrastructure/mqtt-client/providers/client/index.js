70fa57cc93daba2f00a2c97473ff47a4bccc5ba7
miniscandal <oscar01dev@gmail.com>
2026-04-08 20:07:47 -0600

    refactor(mqtt-client): align provider with updated mock flag

--- src/infrastructure/mqtt-client/providers/client/index.js
+++ src/infrastructure/mqtt-client/providers/client/index.js
@@ -3,10 +3,10 @@ import { MqttClientAdapter } from '@infrastructure/mqtt-client/adapters/client';
 import { MqttClientEventNotifier } from '@infrastructure/mqtt-client/notifiers/event';
 import { mqttClientEventSubject } from '@infrastructure/mqtt-client/subjects/mqtt-client-subject';
 
-import { USE_MQTT_MOCK } from '@shared-constants/mqtt-client-config';
+import { USE_MQTT_CLIENT_MOCK } from '@shared-constants/mqtt-client-config';
 
 
-function mqttClientProvider(useMock = USE_MQTT_MOCK) {
+function mqttClientProvider(useMock = USE_MQTT_CLIENT_MOCK) {
     const adapter = useMock ? new MqttClientMockAdapter() : new MqttClientAdapter();
 
     return new MqttClientEventNotifier(adapter, mqttClientEventSubject);
