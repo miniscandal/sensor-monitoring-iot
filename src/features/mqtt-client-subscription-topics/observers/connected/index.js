e2ba83b88c3c9be613986e59f20bd9d29debbdd3
miniscandal <oscar01dev@gmail.com>
2026-04-15 05:42:03 -0600

    feat(mqtt-client-subscription-topics): add subscription to operation-result

--- src/features/mqtt-client-subscription-topics/observers/connected/index.js
+++ src/features/mqtt-client-subscription-topics/observers/connected/index.js
@@ -3,6 +3,7 @@ import { OBSERVER_ENTITY_MQTT_EVENTS } from '@shared-constants/observer-entities
 import { MQTT_CLIENT_EVENT_CONNECT } from '@shared-constants/mqtt-client-events';
 
 import {
+    MQTT_TOPIC_NODE_OPERATION_RESULT,
     MQTT_TOPIC_NODE_DATA,
     MQTT_TOPIC_NODE_STATUS,
     MQTT_TOPIC_NODE_METADATA,
@@ -18,6 +19,7 @@ function OnMqttClientConnectedObserver() {
         listener: ({ actions }) => {
             const { subscribe } = actions;
 
+            subscribe(MQTT_TOPIC_NODE_OPERATION_RESULT);
             subscribe(MQTT_TOPIC_NODE_DATA);
             subscribe(MQTT_TOPIC_NODE_STATUS);
             subscribe(MQTT_TOPIC_NODE_METADATA);
