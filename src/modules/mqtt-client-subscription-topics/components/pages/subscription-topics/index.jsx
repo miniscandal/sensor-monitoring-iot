83f671cdf23d7b9ba97528e3db89efa6faa97053
miniscandal <oscar01dev@gmail.com>
2026-05-12 07:04:22 -0600

    refactor(subscription-topics): drop mqtt observer imports
    - Removed `useSubscribeObserverMqttClient` import
    - Removed `OnMqttClientConnectedObserver` import
    - Deleted observer hook invocation in component

--- src/modules/mqtt-client-subscription-topics/components/pages/subscription-topics/index.jsx
+++ src/modules/mqtt-client-subscription-topics/components/pages/subscription-topics/index.jsx
@@ -1,9 +1,5 @@
 import { useSubscribedTopics } from '@modules/mqtt-client-subscription-topics/hooks/use-subscribed-topics';
 
-import { useSubscribeObserverMqttClient } from '@infrastructure/mqtt-client/hooks/use-subscribe-observer';
-
-import { OnMqttClientConnectedObserver } from '@modules/mqtt-client-subscription-topics/observers/connected';
-
 import { SubscribedTopics } from '../../templates/subscribed-topics';
 
 import './style.css';
@@ -11,8 +7,6 @@ import './style.css';
 
 function MqttClientSubscriptionTopics() {
     const topics = useSubscribedTopics();
-
-    useSubscribeObserverMqttClient(OnMqttClientConnectedObserver());
 
 
     return (
