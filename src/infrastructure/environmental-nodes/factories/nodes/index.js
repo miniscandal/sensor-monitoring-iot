d5b7164778bbf6c4f9ece1a12d9c01e0efd61f4c
miniscandal <oscar01dev@gmail.com>
2026-05-20 15:50:36 -0600

    refactor(env-nodes): update factory to use renamed mock flag constant
    Update `environmentalNodesProviderFactory` to import and consume the shortened
    `USE_ENV_NODE_MOCK` constant instead of the deprecated verbose version.

--- src/infrastructure/environmental-nodes/factories/nodes/index.js
+++ src/infrastructure/environmental-nodes/factories/nodes/index.js
@@ -1,10 +1,10 @@
-import { USE_ENVIRONMENTAL_NODE_MOCK } from '@infrastructure/mqtt-client/constants/client-config';
+import { USE_ENV_NODE_MOCK } from '@infrastructure/mqtt-client/constants/client-config';
 
 import { environmentalNodesMockProvider } from '@mocks/environmental-nodes/providers/nodes';
 import { environmentalNodesProvider } from '@infrastructure/environmental-nodes/providers/nodes';
 
 
-function environmentalNodesProviderFactory(useMock = USE_ENVIRONMENTAL_NODE_MOCK) {
+function environmentalNodesProviderFactory(useMock = USE_ENV_NODE_MOCK) {
 
     return useMock ? environmentalNodesMockProvider : environmentalNodesProvider;
 }
