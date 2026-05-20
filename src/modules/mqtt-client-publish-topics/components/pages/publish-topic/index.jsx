b6e37b4a75426bfd20adda0e5db315e2c46a46e8
miniscandal <oscar01dev@gmail.com>
2026-05-20 15:54:12 -0600

    refactor(publish-page): update imported publishing topic constants to env-node
    Update the `MqttClientPublishTopics` page component to consume the renamed
    `ENV_NODE_COMMAND` and `ALL_ENV_NODE_COMMAND` constants for the topics array.

--- src/modules/mqtt-client-publish-topics/components/pages/publish-topic/index.jsx
+++ src/modules/mqtt-client-publish-topics/components/pages/publish-topic/index.jsx
@@ -5,8 +5,8 @@ import { SubscribedEnvironmentalNodeStatusTopicObserver } from '@modules/mqtt-cl
 import { TopicsPublishes } from '../../templates/topics-publishes';
 
 import {
-    MQTT_TOPIC_ENVIRONMENTAL_NODE_COMMAND,
-    MQTT_TOPIC_ENVIRONMENTAL_NODE_ALL_COMMAND,
+    MQTT_TOPIC_ENV_NODE_COMMAND,
+    MQTT_TOPIC_ALL_ENV_NODE_COMMAND,
 } from '@infrastructure/mqtt-client/constants/client-topics-publishes';
 
 import './style.css';
@@ -14,8 +14,8 @@ import './style.css';
 
 function MqttClientPublishTopics() {
     const topics = [
-        MQTT_TOPIC_ENVIRONMENTAL_NODE_COMMAND(),
-        MQTT_TOPIC_ENVIRONMENTAL_NODE_ALL_COMMAND,
+        MQTT_TOPIC_ENV_NODE_COMMAND(),
+        MQTT_TOPIC_ALL_ENV_NODE_COMMAND,
     ];
 
     useSubscribeObserverMqttClient(SubscribedEnvironmentalNodeStatusTopicObserver());
