9ccfe04d65a7386fb09edc6520b751290cf0d968
miniscandal <oscar01dev@gmail.com>
2026-05-20 15:57:24 -0600

    refactor(mqtt): migrate connected observer to new env-node subscription topics
    Update `OnMqttClientConnectedObserver` to replace legacy node authentication
    and data topics with the newly standardized `ENV_NODE` state (birth/death)
    and telemetry constants.

--- src/modules/mqtt-client-subscription-topics/observers/connected/index.js
+++ src/modules/mqtt-client-subscription-topics/observers/connected/index.js
@@ -3,13 +3,12 @@ import { OBSERVER_ENTITY_MQTT_EVENTS } from '@shared-constants/observer-entities
 import { MQTT_CLIENT_EVENT_CONNECT } from '@infrastructure/mqtt-client/constants/client-events';
 
 import {
-    MQTT_TOPIC_NODE_AUTH_LOGIN,
-    MQTT_TOPIC_NODE_AUTH_LOGOUT,
-    MQTT_TOPIC_NODE_AUTH_SESSION,
-    MQTT_TOPIC_NODE_DATA_SENSOR,
-    MQTT_TOPIC_NODE_METADATA,
-    MQTT_TOPIC_NODE_STATE_CODE,
-    MQTT_TOPIC_NODE_OPERATION_RESULT_CODE,
+    MQTT_TOPIC_ENV_NODE_STATE_BIRTH,
+    MQTT_TOPIC_ENV_NODE_STATE_DEATH,
+    MQTT_TOPIC_ENV_NODE_DATA_SENSOR_ALL,
+    MQTT_TOPIC_ENV_NODE_METADATA,
+    MQTT_TOPIC_ENV_NODE_STATE_CODE,
+    MQTT_TOPIC_ENV_NODE_OP_RESULT_CODE,
 } from '@infrastructure/mqtt-client/constants/client-topics-subscription';
 
 
@@ -21,13 +20,12 @@ function OnMqttClientConnectedObserver() {
         listener: ({ actions }) => {
             const { subscribe } = actions;
 
-            subscribe(MQTT_TOPIC_NODE_AUTH_LOGIN);
-            subscribe(MQTT_TOPIC_NODE_AUTH_LOGOUT);
-            subscribe(MQTT_TOPIC_NODE_AUTH_SESSION);
-            subscribe(MQTT_TOPIC_NODE_DATA_SENSOR);
-            subscribe(MQTT_TOPIC_NODE_METADATA);
-            subscribe(MQTT_TOPIC_NODE_STATE_CODE);
-            subscribe(MQTT_TOPIC_NODE_OPERATION_RESULT_CODE);
+            subscribe(MQTT_TOPIC_ENV_NODE_STATE_BIRTH);
+            subscribe(MQTT_TOPIC_ENV_NODE_STATE_DEATH);
+            subscribe(MQTT_TOPIC_ENV_NODE_DATA_SENSOR_ALL);
+            subscribe(MQTT_TOPIC_ENV_NODE_METADATA);
+            subscribe(MQTT_TOPIC_ENV_NODE_STATE_CODE);
+            subscribe(MQTT_TOPIC_ENV_NODE_OP_RESULT_CODE);
         },
     };
 }
