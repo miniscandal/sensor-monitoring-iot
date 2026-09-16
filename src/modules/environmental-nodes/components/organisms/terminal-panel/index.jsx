a438ae4e6b095d12066cabaf896c5688d79f2abc
miniscandal <oscar01dev@gmail.com>
2026-09-16 17:52:21 -0600

    feat(environmental-nodes): add TerminalPanel organism component
    - Implement TerminalPanel component to render interactive CLI prompts
    - Add command history state management and auto-scroll functionality
    - Include custom CSS styles for layout, scrollbars, and terminal theme

---
+++ src/modules/environmental-nodes/components/organisms/terminal-panel/index.jsx
@@ -0,0 +1,78 @@
+import { useState, useRef, useEffect } from 'preact/hooks';
+
+import { Label } from '@shared-components/atoms/label';
+
+import { PromptCommand } from '../../molecules/prompt-command';
+
+import './style.css';
+
+import { ButtonSvgIcon } from '@shared-components/molecules/button-svg-icon';
+
+
+function TerminalPanel({ nodeId, setSelectedNodeId }) {
+    const [inputCommand, setInputCommand] = useState('');
+    const [history, setHistory] = useState([]);
+
+    const terminalBodyRef = useRef(null);
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (terminalBodyRef.current) {
+            terminalBodyRef.current.scrollTop = terminalBodyRef.current.scrollHeight;
+        }
+    }, [history]);
+
+    const handleSendMessage = (event) => {
+        event.preventDefault();
+
+        setHistory(prevState => [
+            ...prevState,
+            <PromptCommand
+                key={prevState.length}
+                prompt={`nodeId/${nodeId}>\u00A0`}
+                command={`${inputCommand.trim()}\n`}
+            />,
+        ]);
+
+        setInputCommand('');
+
+        console.log(nodeId);
+    };
+
+    const handleInput = (event) => {
+        setInputCommand(event.target.value);
+    };
+
+    const handleTerminalPanelBody = () => {
+        inputRef.current.focus();
+    };
+
+    const handleButtonClose = () => setSelectedNodeId(null);
+
+
+    return (
+        <section class="terminal-panel">
+            <header>
+                <ButtonSvgIcon name="closeSmall" size="tiny" handleClick={handleButtonClose} />
+            </header>
+            <div ref={terminalBodyRef} class="terminal-panel-body" onClick={handleTerminalPanelBody}>
+                <pre>
+                    {history}
+                </pre>
+                <form onSubmit={handleSendMessage}>
+                    <Label htmlFor="terminal-input" text={`nodeId/${nodeId}>\u00A0`} />
+                    <input
+                        id="terminal-input"
+                        autoFocus
+                        ref={inputRef}
+                        type="text"
+                        value={inputCommand}
+                        onInput={handleInput}
+                    />
+                </form>
+            </div>
+        </section>
+    );
+}
+
+export { TerminalPanel };
